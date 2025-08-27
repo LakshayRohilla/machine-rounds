f17dbeaf1768fc5a8fa1418252d36bb90f6509bc
Rohilla <lakshay.rohilla@sms-group.com>
2025-08-27 19:45:13 +0530

    traffic light completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Acordian from './components/accordian/accordian';
+import TrafficLight from './components/trafficLight/trafficLight';
 
 function App() {
   return (
     <div className="App" style={{}}>
       <Acordian/>
+	    <TrafficLight/>
     </div>
   );
 }
